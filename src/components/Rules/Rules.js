c7918a48cb2a40bafd2becfbfa031e576e29e85a
Yuyang Zhou <yuyangzhou99@gmail.com>
2022-11-14 21:12:52 -0500

    Modified Rules

--- src/components/Rules/Rules.js
+++ src/components/Rules/Rules.js
@@ -5,15 +5,25 @@ class Rules extends Component {
     render() {
         return (
             <div className="Rules">
-                <h1>How to play Othello</h1>
-                <p>
-                Othello is a two-player board game played on a square board (size n x n square grid) using bi-colored disks. The object of the game is to maximize the number of disks on the board with your assigned color. 
-                Players take turns placing disks of their color on the board until the game ends. 
-                This happens when it is no longer possible for either of the players to make a legal move or until the board is filled. 
-                A move is legal if the player can place their disk on the board such that one or more of their opponent's rows of disks is bordered at each end by a disk of the player's color.
-                 A row may be made up of one or more disks. When a player places a disk on the board, the disk changes the color of all the opposing player's disks lying on any straight line between the new disk and one of the player's disks. 
-                 When the game ends, the player with more disks of their color on the board wins.
+                <br/>
+                <h1 className='header'>How to play Othello</h1>
+                <p className='p'>
+                Othello is a strategy board game for two players (Black and White), played on an 8 by 8 board. The game traditionally begins with four discs placed in the middle of the board as shown below. Black moves first.
                 </p>
+                <img src="how_to_play_othello_0.png" alt="Othello starting position"/>
+                <p>
+                Black must place a black disc on the board, in such a way that there is at least one straight (horizontal, vertical, or diagonal) occupied line between the new disc and another black disc, with one or more contiguous white pieces between them. In the starting position, Black has the following 4 options indicated by translucent discs:
+                </p>
+                <img src="how_to_play_othello_1.png" alt="Othello starting position"/>
+                <p>After placing the disc, Black flips all white discs lying on a straight line between the new disc and any existing black discs. All flipped discs are now black. If Black decides to place a disc in the topmost location, one white disc gets flipped, and the board now looks like this:</p>
+                <img src="how_to_play_othello_2.png" alt="Othello starting position"/>
+                <p>Now White plays. This player operates under the same rules, with the roles reversed: White lays down a white disc, causing black discs to flip. Possibilities at this time would be:</p>
+                <img src="how_to_play_othello_3.png" alt="Othello starting position"/>
+                <p>If White plays the bottom left option and flips one disc:</p>
+                <img src="how_to_play_othello_4.png" alt="Othello starting position"/>
+                <p>Players alternate taking turns. If a player does not have any valid moves, play passes back to the other player. When neither player can move, the game ends. A game of Othello may end before the board is completely filled.
+
+The player with the most discs on the board at the end of the game wins. If both players have the same number of discs, then the game is a draw.</p>
             </div>
         )
     }
